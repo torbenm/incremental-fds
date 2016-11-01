d0b77ed369132d682389109f919e00e858c1b649
Torben Meyer <torben.meyer@mailbox.org>
2016-11-01 13:10:39 +0100

    Added generic utility tests

--- utils/src/main/java/org/mp/naumann/utils/GenericHelper.java
+++ utils/src/main/java/org/mp/naumann/utils/GenericHelper.java
@@ -7,8 +7,14 @@ public class GenericHelper {
     public static <T> T cast(Object obj, Class<T> clazz){
         if(clazz.isInstance(obj))
             return (T)obj;
-        else
+        else if(isNumberCast(obj, clazz)){
+            return (T)obj;
+        } else
             throw new ClassCastException();
+    }
+
+    public static boolean isNumberCast(Object obj, Class<?> clazz){
+        return Number.class.isInstance(obj) && Number.class.isAssignableFrom(clazz);
     }
 
     public static <T, S> Map<T, S> createGenericMap(Class<? extends Map> mapClass, Class<T> keyClass, Class<S> valueClass){
---
+++ utils/src/test/java/org/mp/naumann/utils/GenericHelperTest.java
@@ -0,0 +1,49 @@
+package org.mp.naumann.utils;
+
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mp.naumann.utils.GenericHelper.cast;
+import static org.mp.naumann.utils.GenericHelper.createGenericMap;
+import static org.mp.naumann.utils.GenericHelper.isNumberCast;
+
+public class GenericHelperTest {
+
+    @Test
+    public void testCorrectCast(){
+        Object s = "demo string";
+        assertTrue(cast(s, String.class) != null);
+    }
+
+    @Test(expected=ClassCastException.class)
+    public void testNullCast(){
+        cast(null, Object.class);
+    }
+
+    @Test
+    public void testInterfaceCast(){
+        Object s = "demo string";
+        assertTrue(cast(s, Serializable.class) != null);
+    }
+
+    @Test
+    public void testIntegerToDoubleCast(){
+        assertTrue(cast(5, Double.class) != null);
+    }
+
+    @Test
+    public void testIsNumberCast(){
+        assertTrue(isNumberCast(5, Double.class));
+        assertFalse(isNumberCast(5, String.class));
+        assertTrue(isNumberCast(5.0f, Integer.class));
+    }
+
+    @Test
+    public void testCreateGenericMap(){
+        assertTrue(createGenericMap(HashMap.class, String.class, Integer.class) instanceof HashMap);
+    }
+}
