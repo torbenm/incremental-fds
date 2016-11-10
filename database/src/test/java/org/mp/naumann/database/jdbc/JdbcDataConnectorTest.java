bbc5469ffc6b4375f210908eaa074ed41193c88b
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2016-11-10 07:47:59 +0100

    using assertEquals rather than assertTrue

--- database/src/test/java/org/mp/naumann/database/jdbc/JdbcDataConnectorTest.java
+++ database/src/test/java/org/mp/naumann/database/jdbc/JdbcDataConnectorTest.java
@@ -3,26 +3,23 @@ package org.mp.naumann.database.jdbc;
 import org.junit.Test;
 import org.mp.naumann.database.Table;
 
-import java.io.File;
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class JdbcDataConnectorTest extends JdbcTest {
-
-
 
     @Test
     public void testGetTableNames() {
         List<String> tableNames = connector.getTableNames();
-        assertTrue(tableNames.size() == 1);
-        assertTrue(tableNames.get(0).equals(testTableName));
+        assertEquals(tableNames.size(), 1);
+        assertEquals(tableNames.get(0), testTableName);
     }
 
     @Test
     public void testGetTable() {
         Table table = connector.getTable(testTableName);
-        assertTrue(table.getName().equals(testTableName));
+        assertEquals(table.getName(), testTableName);
     }
 }
 
