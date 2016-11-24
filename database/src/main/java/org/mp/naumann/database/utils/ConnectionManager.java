bf275272b4db7ef7674bbadf4f555f3105a8d4d5
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2016-11-24 11:37:47 +0100

    moved preparing postgres connection info to ConnectionManager

--- database/src/main/java/org/mp/naumann/database/utils/ConnectionManager.java
+++ database/src/main/java/org/mp/naumann/database/utils/ConnectionManager.java
@@ -3,8 +3,12 @@ package org.mp.naumann.database.utils;
 import org.mp.naumann.database.ConnectionException;
 import org.mp.naumann.database.jdbc.ConnectionInfo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class ConnectionManager {
 
@@ -22,8 +26,31 @@ public class ConnectionManager {
         return connection;
     }
 
+    private static ConnectionInfo getPostgresConnectionInfo() throws ConnectionException {
+        ConnectionInfo ci = new ConnectionInfo();
+        Properties properties = new Properties();
+        URL settingsURL = ConnectionInfo.class.getClassLoader().getResource("properties.xml");
+        if (settingsURL == null) settingsURL = ConnectionInfo.class.getClassLoader().getResource("properties.default.xml");
+        if (settingsURL != null) {
+            File settings = new File(settingsURL.getFile());
+            try {
+                try (FileInputStream input = new FileInputStream(settings)) {
+                    properties.loadFromXML(input);
+                    int port = Integer.parseInt(properties.getProperty("port"));
+                    String database = properties.getProperty("database");
+                    ci.user = properties.getProperty("user");
+                    ci.pass = properties.getProperty("pass");
+                    ci.connectionString = "jdbc:postgresql://localhost:" + Integer.toString(port) + "/" + database;
+                    return ci;
+                }
+            } catch (Exception e) {
+                throw new ConnectionException(e);
+            }
+        } else throw new ConnectionException("Neither default nor custom properties file found.");
+    }
+
     public static Connection getPostgresConnection() throws ConnectionException {
-        return getConnection("org.postgresql.Driver", PostgresConnection.getConnectionInfo());
+        return getConnection("org.postgresql.Driver", getPostgresConnectionInfo());
     }
 
     public static Connection getCsvConnection(String csvDir, String separator) throws ConnectionException {
