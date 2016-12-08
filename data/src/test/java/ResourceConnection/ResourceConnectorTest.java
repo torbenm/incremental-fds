bf7e265634bda4bc2f15cad30641e1c299876cdf
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2016-12-08 07:56:38 +0100

    fixed ResourceConnectorTest on Windows (slash vs backslash in paths)

--- data/src/test/java/ResourceConnection/ResourceConnectorTest.java
+++ data/src/test/java/ResourceConnection/ResourceConnectorTest.java
@@ -1,45 +1,31 @@
 package ResourceConnection;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by dennis on 01.12.16.
- */
 public class ResourceConnectorTest {
-
-    private static ResourceConnector rc;
-
-    @BeforeClass
-    public static void setUp() throws IOException {
-        rc = ResourceConnector.getInstance();
-    }
 
     @Test
     public void getsCSVWithCorrectType() {
-        InputStream testInputStream = rc.getResource(ResourceType.TEST, "test.unit.csv");
+        InputStream testInputStream = ResourceConnector.getResource(ResourceType.TEST, "test.unit.csv");
         assertNotNull("InputStream should not be null", testInputStream);
     }
 
     @Test
     public void returnsNullWithWrongType() {
-        InputStream testInputStream = rc.getResource(ResourceType.BASELINE, "test.unit.csv");
+        InputStream testInputStream = ResourceConnector.getResource(ResourceType.BASELINE, "test.unit.csv");
         assertNull("InputStream should be null", testInputStream);
     }
 
     @Test
     public void getsPathOfCSVWithCorrectType() {
-        String path = rc.getResourcePath(ResourceType.TEST, "test.unit.csv");
-        assertEquals("Path should be equal", System.getProperty("user.dir") + "/target/test-classes/ResourceConnection/test/test.unit.csv", path);
+        File actual = new File(ResourceConnector.getResourcePath(ResourceType.TEST, "test.unit.csv"));
+        File expected = new File(System.getProperty("user.dir") + "/target/test-classes/ResourceConnection/test/test.unit.csv");
+        assertEquals("Path should be equal", expected.getPath(), actual.getPath());
     }
-
-
 
 }
