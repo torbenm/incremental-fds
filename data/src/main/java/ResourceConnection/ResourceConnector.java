31adeb85d3efc7c6995901412b4effe628279c54
Daniel Neuschäfer-Rube <daniel.neuschaefer@gmail.com>
2017-02-02 14:46:21 +0100

    moved resources to fixed folder, fixed ResourceConnector accordingly

--- data/src/main/java/ResourceConnection/ResourceConnector.java
+++ data/src/main/java/ResourceConnection/ResourceConnector.java
@@ -1,31 +1,31 @@
 package ResourceConnection;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URL;
 
 public class ResourceConnector {
 
-    public static final String TEST = "test/";
-    public static final String BASELINE = "baseline/";
-    public static final String UPDATE = "update/";
-    public static final String BENCHMARK = "benchmark/";
-    public static final String FULL_BATCHES = "full_batches/";
+    public static final String TEST = "../data_files/test/";
+    public static final String BASELINE = "../data_files/baseline/";
+    public static final String UPDATE = "../data_files/update/";
+    public static final String BENCHMARK = "../data_files/benchmark/";
+    public static final String FULL_BATCHES = "../data_files/full_batches/";
 
     public static InputStream getResource(String folder, String filename) {
-        return ResourceConnector.class.getResourceAsStream(folder + "/" + filename);
-    }
-
-    private static String getResourceIfExists(URL url) {
-        return (url == null) ? "" : url.getPath();
+        try {
+            return new FileInputStream(getResourcePath(folder, filename));
+        } catch (FileNotFoundException e) {
+            return null;
+        }
     }
 
     public static String getResourcePath(String folder, String filename) {
-        return getResourcePath(folder + "/" + filename);
+        return getResourcePath((folder + "/" + filename).replace("//", "/"));
     }
 
     public static String getResourcePath(String filename) {
-        URL url = ResourceConnector.class.getResource(filename);
-        return getResourceIfExists(url);
+        return filename;
     }
 
 }
