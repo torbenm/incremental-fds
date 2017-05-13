b66d3d02cd489587f2f97f268cf268cd2b5d25e3
Philipp Schirmer <philipp@schirmer-online.de>
2017-05-13 12:56:38 +0200

    fix update

--- algorithms-fd/src/main/java/org/mp/naumann/algorithms/fd/incremental/datastructures/AbstractStatementApplier.java
+++ algorithms-fd/src/main/java/org/mp/naumann/algorithms/fd/incremental/datastructures/AbstractStatementApplier.java
@@ -11,44 +11,52 @@ import org.mp.naumann.database.statement.StatementVisitor;
 import org.mp.naumann.database.statement.UpdateStatement;
 
 public abstract class AbstractStatementApplier implements StatementVisitor {
-    private final Set<Integer> inserted = new HashSet<>();
-    private final Set<Integer> deleted = new HashSet<>();
 
-    @Override
-    public void visit(DeleteStatement delete) {
-        Benchmark benchmark = Benchmark.start("Delete", Benchmark.DEFAULT_LEVEL + 7);
-        Collection<Integer> removed = removeRecord(delete.getValueMap());
-        deleted.addAll(removed);
-        benchmark.finish();
-    }
+	private final Set<Integer> inserted = new HashSet<>();
+	private final Set<Integer> deleted = new HashSet<>();
 
-    @Override
-    public void visit(UpdateStatement update) {
-        Benchmark benchmark = Benchmark.start("Update", Benchmark.DEFAULT_LEVEL + 7);
-        Collection<Integer> removed = removeRecord(update.getOldValueMap());
-        deleted.addAll(removed);
-        int insertedRecord = addRecord(update.getNewValueMap());
-        inserted.add(insertedRecord);
-        benchmark.finish();
-    }
+	@Override
+	public void visit(DeleteStatement delete) {
+		Benchmark benchmark = Benchmark.start("Delete", Benchmark.DEFAULT_LEVEL + 7);
+		delete(delete.getValueMap());
+		benchmark.finish();
+	}
 
-    @Override
-    public void visit(InsertStatement insert) {
-        Benchmark benchmark = Benchmark.start("Insert", Benchmark.DEFAULT_LEVEL + 7);
-        int insertedRecord = addRecord(insert.getValueMap());
-        inserted.add(insertedRecord);
-        benchmark.finish();
-    }
+	@Override
+	public void visit(UpdateStatement update) {
+		Benchmark benchmark = Benchmark.start("Update", Benchmark.DEFAULT_LEVEL + 7);
+		Collection<Integer> removed = delete(update.getOldValueMap());
+		removed.forEach(id -> insert(update.getNewValueMap()));
+		benchmark.finish();
+	}
 
-    public Set<Integer> getInserted() {
-        return inserted;
-    }
+	private Collection<Integer> delete(Map<String, String> oldValueMap) {
+		Collection<Integer> removed = removeRecord(oldValueMap);
+		deleted.addAll(removed);
+		return removed;
+	}
 
-    public Set<Integer> getDeleted() {
-        return deleted;
-    }
+	private void insert(Map<String, String> newValueMap) {
+		int insertedRecord = addRecord(newValueMap);
+		inserted.add(insertedRecord);
+	}
 
-    protected abstract int addRecord(Map<String, String> valueMap);
+	@Override
+	public void visit(InsertStatement insert) {
+		Benchmark benchmark = Benchmark.start("Insert", Benchmark.DEFAULT_LEVEL + 7);
+		insert(insert.getValueMap());
+		benchmark.finish();
+	}
 
-    protected abstract Collection<Integer> removeRecord(Map<String, String> valueMap);
+	public Set<Integer> getInserted() {
+		return inserted;
+	}
+
+	public Set<Integer> getDeleted() {
+		return deleted;
+	}
+
+	protected abstract int addRecord(Map<String, String> valueMap);
+
+	protected abstract Collection<Integer> removeRecord(Map<String, String> valueMap);
 }
